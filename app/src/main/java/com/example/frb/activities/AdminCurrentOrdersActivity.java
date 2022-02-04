ba85908b4b124cdb5eb56ed50e04f9c15398cc17
TE B 39 Jay Joshi <jayjoshi112711@gmail.com>
2022-02-04 16:41:05 +0530

    Added periodic refresh for Current Orders Activity

--- app/src/main/java/com/example/frb/activities/AdminCurrentOrdersActivity.java
+++ app/src/main/java/com/example/frb/activities/AdminCurrentOrdersActivity.java
@@ -18,6 +18,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class AdminCurrentOrdersActivity extends AppCompatActivity {
     DatabaseReference dbref;
@@ -26,6 +28,9 @@ public class AdminCurrentOrdersActivity extends AppCompatActivity {
     private ArrayList<String> amount;
     private ArrayList<String> token;
     private ArrayList<String> status;
+
+    private Timer autoUpdate;
+
     RecyclerView recyclerView;
     CurrentOrdersAdapter currentOrdersAdapter;
     LinearLayoutManager linearLayoutManager;
@@ -35,17 +40,18 @@ public class AdminCurrentOrdersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_current_orders);
 
-        token = new ArrayList<>();
+/*        token = new ArrayList<>();
         transactionId = new ArrayList<>();
 //        phone = new ArrayList<>();
         time = new ArrayList<>();
         amount = new ArrayList<>();
-        status = new ArrayList<>();
+        status = new ArrayList<>();*/
+
 
         dbref = FirebaseDatabase.getInstance().getReference();
 
         //initDataset();
-        Log.i("after initializing", String.valueOf((transactionId.size() )));
+//        Log.i("after initializing", String.valueOf((transactionId.size() )));
 
         linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setReverseLayout(false);
@@ -54,6 +60,64 @@ public class AdminCurrentOrdersActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         /*CurrentOrdersAdapter = new CurrentOrdersAdapter(transactionId, phone, time, amount);
         recyclerView.setAdapter(CurrentOrdersAdapter);*/
+
+        /*dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.child("Status").getChildren()){
+//                    Bill temp = dataSnapshot.getValue(Bill.class);
+                    String id = dataSnapshot.getKey();
+                    transactionId.add(id);
+                    String statusUpdate = dataSnapshot.getValue(String.class);
+                    Log.i("Logger", statusUpdate);
+                    status.add(statusUpdate);
+
+                    Bill bill = snapshot.child("Bill").child(id).getValue(Bill.class);
+                    token.add(bill.getToken().toString());
+                    time.add(bill.getTime());
+                    amount.add(bill.getTotalPrice());
+
+                    Log.i("Check", "transaction"+transactionId);
+
+                }
+                currentOrdersAdapter = new CurrentOrdersAdapter(AdminCurrentOrdersActivity.this, transactionId, status, time, amount, token);
+                recyclerView.setAdapter(currentOrdersAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });*/
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        autoUpdate = new Timer();
+        autoUpdate.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        updateHTML();
+                    }
+                });
+            }
+        }, 0, 10000);
+        recyclerView.setAdapter(currentOrdersAdapter);
+
+    }
+
+    private void updateHTML() {
+        token = new ArrayList<>();
+        transactionId = new ArrayList<>();
+//        phone = new ArrayList<>();
+        time = new ArrayList<>();
+        amount = new ArrayList<>();
+        status = new ArrayList<>();
 
         dbref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -84,14 +148,19 @@ public class AdminCurrentOrdersActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onPause() {
+        super.onPause();
+        token = new ArrayList<>();
+        transactionId = new ArrayList<>();
+//        phone = new ArrayList<>();
+        time = new ArrayList<>();
+        amount = new ArrayList<>();
+        status = new ArrayList<>();
+        currentOrdersAdapter = new CurrentOrdersAdapter(AdminCurrentOrdersActivity.this, transactionId, status, time, amount, token);
         recyclerView.setAdapter(currentOrdersAdapter);
 
     }
-
 }
