563a98bf37b1e3a90df4eca6aba2beaf648a1266
TE B 39 Jay Joshi <jayjoshi112711@gmail.com>
2022-05-28 17:38:14 +0530

    Base Payment Details Activity in Sign Up created

---
+++ app/src/main/java/com/example/frb/activities/SignUpPaymentDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.frb.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.frb.R;
+
+public class SignUpPaymentDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_payment_details);
+    }
+}
